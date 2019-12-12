f016d33ebbb098bf5de168a19178e7460cce6417
Ceara Owre <cearaowre@hotmail.com>
2019-12-12 16:04:22 -0500

    Updates to modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -23,21 +23,28 @@ module.exports = function (app) {
     var surveyResults = req.body;
     var surveyScore = surveyResults.scores;
 
-    // console.log("This is here: " +surveyScore);
+    console.log("surveyResults.scores"+ surveyResults.scores);
 
-    // Variable
+    // Set up variable to determine difference in scores to create match
     var difference = 0;
 
+    // For loop - loop through each potential match in friends array
     for (var i = 0; i < friends.length; i++){
-        console.log(friends[i]);
+        console.log(friends[i].name);
         difference = 0;
-
+        // Nested for loop - loop through score array
         for (var n = 0; n < friends[i].scores[n]; n++) {
+            console.log("friends[i].scores[n] is: " + friends[i].scores[n]);
             difference += Math.abs(parseInt(surveyScore[n]) - parseInt(friends[i].scores[n]));
+            console.log("surveyScore[n] is: " + surveyScore[n]);
+            console.log("friends[i].scores[n] is: " + friends[i].scores[n]);
+            console.log("difference is: " + difference);
+            console.log("freindDifference is: " + match.friendDifference);
 
             if (difference <= match.friendDifference) {
                 match.name =  friends[i].name;
                 match.photo = friends[i].photo;
+                match.friendDifference = difference;
             }
         }
     }
